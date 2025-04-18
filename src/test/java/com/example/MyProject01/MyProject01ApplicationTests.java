230a57588047d29d1ce4cd33367228d9953b2228
cylin0201 <cylin0201@naver.com>
2025-04-18 13:03:44 +0900

    First commit

---
+++ src/test/java/com/example/MyProject01/MyProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyProject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
